a72d9b002b17ec8a85ef67d5387ba6f6f91c105e
Flynnlee <593029372@qq.com>
2019-11-10 19:41:38 +0800

    fix: fix ts type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,10 @@
 // 1. 确保在声明补充的类型之前导入 'vue'
-import VueRouter, { Location, Route, NavigationGuard } from 'vue-router'
+import VueRouter, { Location, Route, RawLocation } from 'vue-router'
 
 export class VueRouterPlus extends VueRouter {
   to: Route
   from: Route
-  next: NavigationGuard
+  next: (to?: RawLocation | false | ((vm: any) => any) | void) => void
 }
 
 export default VueRouterPlus
