b509abb995a7bd202691c8f68375743224c38681
FlynnLee <593029372@qq.com>
2019-12-03 13:56:45 +0800

    docs: update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,35 @@
 // 1. 确保在声明补充的类型之前导入 'vue'
-import VueRouter, { Location, Route, RawLocation } from 'vue-router'
+import VueRouter, {
+  Location,
+  Route,
+  RawLocation,
+  RouteConfig as VueRouteConfig
+} from 'vue-router'
 
 export class VueRouterPlus extends VueRouter {
   to: Route
   from: Route
   next: (to?: RawLocation | false | ((vm: any) => any) | void) => void
   isHistoryBF: Boolean
+}
+
+interface QueryOption {
+  default?: number | string
+  type: NumberConstructor | StringConstructor
+}
+
+export interface QueryOptions {
+  [queryName: string]: QueryOption
+}
+
+export interface RouteConfigMeta {
+  [key: string]: any
+  queryOptions?: QueryOptions
+}
+
+export interface RouteConfig extends VueRouteConfig {
+  children?: RouteConfig[]
+  meta?: RouteConfigMeta
 }
 
 export { Location, Route, RawLocation }
